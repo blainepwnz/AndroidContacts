063ae07a224099b4df5a42e1b57fb642c50b3de2
andrewtomash <kpubedko@gmail.com>
2017-02-26 18:38:34 +0200

    removed unused dependencies 1.0.0 release

--- app/src/main/java/com/tomash/contactgetter/MainActivity.java
+++ app/src/main/java/com/tomash/contactgetter/MainActivity.java
@@ -1,30 +1,23 @@
 package com.tomash.contactgetter;
 
-import android.os.Bundle;
-import android.support.v7.app.AppCompatActivity;
-import android.view.View;
-import android.widget.ImageView;
+public class MainActivity {
 
-import com.tomash.contactgetter.main.ContactsGetter;
-
-public class MainActivity extends AppCompatActivity {
-
-    @Override
-    protected void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
-//        mHandler = new ContactsGetter(this);
-        final ImageView photoIv = (ImageView) findViewById(R.id.photo_iv);
-
-        findViewById(R.id.sniff_btn).setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                System.out.println(123);
-                new ContactsGetter.Builder(MainActivity.this)
-                    .allFields()
-                    .buildList();
-                System.out.println(12345);
-            }
-        });
-    }
+//    @Override
+//    protected void onCreate(Bundle savedInstanceState) {
+//        super.onCreate(savedInstanceState);
+//        setContentView(R.layout.activity_main);
+////        mHandler = new ContactsGetter(this);
+//        final ImageView photoIv = (ImageView) findViewById(R.id.photo_iv);
+//
+//        findViewById(R.id.sniff_btn).setOnClickListener(new View.OnClickListener() {
+//            @Override
+//            public void onClick(View view) {
+//                System.out.println(123);
+//                new ContactsGetter.Builder(MainActivity.this)
+//                    .allFields()
+//                    .buildList();
+//                System.out.println(12345);
+//            }
+//        });
+//    }
 }
